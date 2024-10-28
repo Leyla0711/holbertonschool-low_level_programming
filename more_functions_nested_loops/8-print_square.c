09ae24fc4a19b5eca51017999848fe528b6b6c1f
Leyla0711 <9994@holbertonestudents.com>
2024-10-28 12:47:05 +0000

    answears

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square -  - printing square
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;j
+		for (i = 0; i < n; i++)
+	{
+		_putchar"#"
+	}
+	_putchar('\n');
+}
