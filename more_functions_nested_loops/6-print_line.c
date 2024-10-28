f5193e17da9ca16975765cb22bdfb45ee5506406
Leyla0711 <9994@holbertonestudents.com>
2024-10-28 12:19:31 +0000

    answears

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
- *  print_line - printing lines
+ * print_line - printing lines
  * @n: number of times
  *
  * Return: void
- */void print_line(int n)
+ */
+
+
+void print_line(int n)
 {
 	int i;
 
