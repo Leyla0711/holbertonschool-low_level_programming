95ea83ef1a756c3a1aa2bf9d65003f4a4d6ae118
Leyla0711 <9994@holbertonestudents.com>
2024-10-28 11:58:33 +0000

    answears

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints
-* owned by Bwave ICT
-*Return: void
-*/
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
+ */
 
-void print_most_numbers(void)
+void more_numbers(void)
 {
-	char c;{
 
-
-		def print_ten_times_numbers():
-    for i in range(15):  # Range goes from 0 to 14
-        print(i * 10)
-
-if __name__ == "__main__":
-    print_ten_times_numbers()
-
-	_putchar(c);
-	}
-	_putchar('\n');
+int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
 }
-
