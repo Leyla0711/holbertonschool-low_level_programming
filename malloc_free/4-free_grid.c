b924da1ece3cd0acb474de5fd528ee83773aee77
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-19 10:09:04 +0000

    answears

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a 2D grid previously created
+ * @grid: input pointer to grid or 2D array
+ * @height: height of 2D arrray
+ * Return: void
+ */
 
 void free_grid(int **grid, int height)
 {
