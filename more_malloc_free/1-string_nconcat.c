9a586d184162406fdf335041d1fce960b9ce04cb
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-19 12:11:19 +0000

    answears

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,12 @@
-#include "main.h"
+#include "mainn.h"
 #include <stdlib.h>
-
 /**
 * string_nconcat - prints concatenate string;
 * @s1: input string.
 * @s2: input string.
 * @n: len s2 string for print.
-*
-* Return: NULL
+* Return: Nothing.
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int l1, i, e;
@@ -17,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	
 	l1 = 0;
 	while (s1[l1])
 		l1++;
