5c3e0d418320a41d604485557af2291a7d6c7b5d
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-19 12:36:15 +0000

    answears

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - prints calloc;
+* @nmemb: input string.
+* @size: input string.
+* Return: Pointer.
+* If malloc fails, then _calloc returns NULL
+*/
+
+
