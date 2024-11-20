5346bb33f7b0caf13dc18eca09335d393a80bbc4
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-20 10:22:50 +0000

    correcting errors 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -6,29 +6,25 @@
 * @nmemb: input string.
 * @size: input string.
 * Return: Pointer.
-* If malloc fails, then _calloc returns NULL
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
+
 {
-    char *p;
-    unsigned int d, i;
+	char *p;
+	unsigned int i, d;
 
-    d = nmemb * size;
+	d = nmemb * size;
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	p = malloc(d);
+	if (p == NULL)
+		return (NULL);
 
-    p = malloc(d);
-
-    if (p == NULL)
-        return (NULL);
-
-    for (i = 0; i < d; i++)
-    {
-        p[i] = 0;
-    }
-
-    return (p);
+	for (i = 0; i < (d); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
 }
-
