f10d52934901d89dfaad6cf8958c4be9e6922502
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-20 12:55:42 +0000

    answers?

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,25 +11,24 @@
  */
 
 int *array_range(int min, int max)
+
 {
-        int *a;
-        int i, full;
+	int *a;
+	int i, full;
 
-        full = (max - min) + 1;
-        if (min > max)
-        {
-                return (NULL);
-        }
-        a = malloc(sizeof(int) * full);
-        if (a == NULL)
-        {
-                return (NULL);
-        }
-
-        for (i = 0; min <= max; i++)
-        {
-                a[i] = min++;
-        }
-
-        return (a);
+	full = (max - min) + 1;
+	if (min > max)
+	{
+		return (NULL);
+	}
+	a = malloc(sizeof(int) * full);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; min <= max; i++)
+	{
+		a[i] = min++;
+	}
+	return (a);
 }
