ab91d7753dd27d4d7deaba7f36085abe77f12803
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 10:37:19 +0000

    answears

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - dog data
  * @name: name of the dog
@@ -9,16 +8,13 @@
  *
  * Description: Defines metadata about the dog and type of dog's struct
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
-} dog_typ;
-
-
+} dog_t;
+/* Additional function prototypes */
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
