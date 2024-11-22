e67569dfeac7cddc89483ac0662495a46702285b
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 10:22:19 +0000

    first try

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * define the new type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t
