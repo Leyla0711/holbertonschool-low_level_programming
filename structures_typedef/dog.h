e0b806a917f512a0e7e43a74d676bc3ac99253d5
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 13:08:03 +0000

    answears

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,10 +9,20 @@
  * Description: Defines metadata about the dog and type of dog's stri
  */
 
-typedef struct dog
-
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
