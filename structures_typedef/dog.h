82e4e40d605eb3aad3ba5aaaf5b7126b52dfabba
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 13:03:34 +0000

    answears

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,5 +6,16 @@
  * @age: age of the dog
  * @owner: name of the owner of the dog
  *
- * Description: Defines metadata about the dog and type of dog's stri:wq
- 
+ * Description: Defines metadata about the dog and type of dog's stri
+ */
+
+struct dog;
+{
+       char->*name;
+       float->age;
+       char->*owner;
+}
+
+typedef struct dog_t;
+
+#endif
