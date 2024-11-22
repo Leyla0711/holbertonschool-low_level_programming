e88e8647e007330d826e7f4e09efc6ccd260a379
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 13:10:11 +0000

    answears

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,11 +2,12 @@
 #define DOG_H
 
 /**
+ * struct dog - dog data
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
  *
- * Description: Defines metadata about the dog and type of dog's stri
+ * Description: Defines metadata about the dog and type of dog's struct
  */
 
 struct dog
