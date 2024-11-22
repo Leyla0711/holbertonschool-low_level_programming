b881029698600c712de63a6e62e22332dfd990fa
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 10:27:28 +0000

    aror correction

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,9 +10,16 @@
  * define the new type
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }dog_t
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
