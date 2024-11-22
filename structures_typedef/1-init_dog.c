bffdcffca4561978a36f96d5f0860de2166a3ed0
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-22 11:23:49 +0000

    error correction

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,17 +7,17 @@
  * @name: pointer to the name of the dog
  * @age: age of dog
  * @owner: pointer to owner of dog
- * Return: alweys 0
+ *
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-	if(d)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
-
 }
