a7c93b5cb563db7c9ee2f5f2deacdf24192c82cf
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-23 07:32:18 +0000

    errore

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,21 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+
 /**
- * struct dog - dog info
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
-void _putchar(char c);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 #endif
