02603661a47abdfcdc230752556bc8f4faa85060
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-23 08:03:24 +0000

    first try

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,11 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
   * free_dog - frees any memory allocated by dog.
   * @d: pointer to struct
-  * Return: what does it return?
+  * Return: void
   */
+
 void free_dog(dog_t *d)
+
 {
 	if (d)
 	{
