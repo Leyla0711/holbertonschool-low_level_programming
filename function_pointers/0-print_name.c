4d5c18c232797bc69021c051adbbffee81645fd5
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-25 11:48:39 +0000

    answears

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name 
- * @name: code to print a name
- * @f: pointer to function
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
  */
 
 void print_name(char *name, void (*f)(char *))
-
 {
 	f(name);
 }
