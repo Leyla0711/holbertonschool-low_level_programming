6bf3dee31578be2ae1a64bf1e04649e405479987
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-11-25 12:24:57 +0000

    answears

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,15 +1,25 @@
 #include "function_pointers.h"
 
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned int i = 0;
+void array_iterator(int *array, size_t size, void (*action)(int));
 
-	if (array != NULL && action != NULL && size > 0)
-	{
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
-	}
+/**
+  * array_iterator - ...
+  * @array: ...
+  * @size: ...
+  * @action: ...
+  *
+  * Return: ...
+  */
+
+{
+        unsigned int i = 0;
+
+        if (array != NULL && action != NULL && size > 0)
+        {
+                while (i < size)
+                {
+                        action(array[i]);
+                        i++;
+                }
+        }
 }
