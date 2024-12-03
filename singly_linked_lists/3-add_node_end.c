7c62054f977d26b395d4693e1ee78282af184881
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-12-03 11:25:03 +0000

    function that adds new note at the end 2

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,63 +1,49 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "lists.h"
 
 /**
-  * add_node_end - Adds a new node at the end of a list
-  * @head: The original linked list
-  * @str: The string to add to the node
-  *
-  * Return: The address of the new list or NULL if it failed
-  */
-list_t *add_node_end(list_t **head, const char *str)
+ * _strlen - Returns the length of a string
+ * @s: String being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(const char *s)
 {
-	list_t *new_list, *temp;
+	unsigned int i = 0;
 
-	if (str != NULL)
-	{
-		new_list = malloc(sizeof(list_t));
-		if (new_list == NULL)
-			return (NULL);
-
-		new_list->str = strdup(str);
-		new_list->len = _strlen(str);
-		new_list->next = NULL;
-
-		if (*head == NULL)
-		{
-			*head  = new_list;
-			return (*head);
-		}
-		else
-		{
-			temp = *head;
-			while (temp->next)
-				temp = temp->next;
-
-			temp->next = new_list;
-			return (temp);
-		}
-	}
-
-	return (NULL);
+	while (*s++)
+		++i;
+	return (i);
 }
 
 /**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
+ * add_node_end - Adds a node at the end of a list
+ * @head: Pointer to address of a list
+ * @str: Pointer to string
+ *
+ * Return: Address of new element
+ * On error, NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
 {
-	int c = 0;
+	list_t *new;
+	list_t *old = *head;
 
-	while (*s)
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+	if (!*head)
 	{
-		s++;
-		c++;
+		*head = new;
+		return (*head);
 	}
-
-	return (c);
+	while (old->next)
+		old = old->next;
+	old->next = new;
+	return (new);
 }
