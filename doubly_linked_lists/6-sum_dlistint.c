846d362d10858ef7d0ee848300a5105771e5d0b7
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-12-03 17:17:38 +0000

    answears

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
+
 /**
-  * sum_dlistint - sums all nodes in a linked list
-  * @head: head of the list
-  * Return: the sum of the nodes
-  */
+ * sum_dlistint - get the sum of all the data `n` of a `dlistint_t` LL
+ * @head: head of doubly LL
+ * Return: sum of all `n` or 0 if LL is empty
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum;
+	dlistint_t *tmp;
 
-	while (head)
-	{
-		sum += head->n;
-		head = head->next;
-	}
+	tmp = head;
+	for (sum = 0; tmp; tmp = tmp->next)
+		sum += tmp->n;
+
 	return (sum);
 }
