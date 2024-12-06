6750a5772f5d5c0a8c2e2420408807a0977547cc
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-12-06 14:39:35 +0000

    answeras

--- file_io/main.h
+++ file_io/main.h
@@ -1,21 +1,17 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
-#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-/* POSIX standards */
-#include <unistd.h>
-
-/* OPEN, READ, WRITE, CLOSE */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
-
-#endif /* _MAIN_H */
+#endif
